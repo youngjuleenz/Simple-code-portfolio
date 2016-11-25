937515f19af060bb273201f99d57f7a86db67e76
youngjuleenz <ylee447@aucklanduni.ac.nz>
2016-11-25 23:48:55 +1300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+﻿var $aboutTitle = $('.about-myself .greeting h2');
+var $developmentWrapper = $('.development-wrapper');
+var developmentIsVisible = false;
+
+$('.background .headerText').delay(500).animate({
+  'opacity': '1',
+  'top': '50%'
+}, 1000);
+
+$(window).scroll(function() {
+
+  var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+  if (bottom_of_window > $aboutTitle.offset().top + $aboutTitle.outerHeight()) {
+    $('.about-myself .greeting h2').addClass('aboutTitleVisible');
+  }
+})
